65424c4981007901815e723b8221638f5cfae7c8
Grellinn <grellinn@gmail.com>
2015-05-09 14:18:04 +0000

    AdminWebDal, IAdminWebDal ofl.

---
+++ AdminWeb/DAL/AdminWebDal.cs
@@ -0,0 +1,44 @@
+﻿using AdminWeb.DAL.Connections;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AdminWeb.DAL
+{
+	public class AdminWebDal : IAdminWebDal, IDisposable
+	{
+		#region Initialize
+		private AdminWebDalDataContext adminWebContext;
+
+		public AdminWebDal(AdminWebDalDataContext adminWebContext)
+		{
+			this.adminWebContext = adminWebContext;
+		}
+		#endregion
+
+
+
+		#region Dispose
+		private bool disposed = false;
+
+		protected virtual void Dispose(bool disposing)
+		{
+			if (!this.disposed)
+			{
+				if (disposing)
+				{
+					adminWebContext.Dispose();
+				}
+			}
+			this.disposed = true;
+		}
+
+		public void Dispose()
+		{
+			Dispose(true);
+			GC.SuppressFinalize(this);
+		}
+		#endregion
+	}
+}
